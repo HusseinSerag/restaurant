3839cf950ae136c6f33a22c074ca1e659063ad14
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-12-04 19:40:49 +0200

    Implemented header bar
    I hardcoded the header for case of handling the tab switching inside of
    index.js, header is now implemented as it is , with a subtle visual cue
    that shows that someone actually chose a tab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './styles/styles.css'
-import {default as head} from './header.js'
+
 //initial page 
-const content = document.querySelector('#content')
-content.appendChild(head())
+const content = document.querySelector('#content');
+const menu = document.querySelector('[class$=menu]');
+const home = document.querySelector('[class$=home]');
+const contact = document.querySelector('[class$=contact]');
+[menu,contact,home].forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        menu.classList.remove('chosen-tab')
+        contact.classList.remove('chosen-tab')
+        home.classList.remove('chosen-tab')
+        Array.from(content.children).forEach(child=>content.removeChild(child))
+        tab.classList.add('chosen-tab')
+        content.appendChild()
+    })
+    
+})
