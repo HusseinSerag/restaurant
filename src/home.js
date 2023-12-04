c3e3589f36104af939bf648f7c4368e2e8aec9ba
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-12-04 21:18:36 +0200

    Finish home.js
    home.js rendering is working completely right, renders to menu tab when
    user presses the menu. Maybe font , color may change in the future
    commits

--- src/home.js
+++ src/home.js
@@ -1,7 +1,35 @@
 import './styles/styles.css'
-
+import * as Controller from './index'
+import { menuTab } from './menu'
 export function homeTab(){
     let container = document.createElement('div')
-    container.textContent ='I am from home'
+    container.classList.add('homeContainer')
+    let title =document.createElement('h1')
+    let subTitle = document.createElement('h4')
+    let titleContainer = document.createElement('div')
+    titleContainer.appendChild(title)
+    titleContainer.appendChild(subTitle)
+    title.textContent = 'Burgeria'
+    subTitle.textContent = 'the best burger in town!'
+    titleContainer.classList.add('titleContainer')
+    container.appendChild(titleContainer)
+
+    let menuOption = document.createElement('button')
+    menuOption.textContent = 'Our menu'
+    menuOption.className = 'btn menu-btn'
+    menuOption.addEventListener('click',()=>{
+        Controller.menu.classList.remove('chosen-tab')
+        Controller.contact.classList.remove('chosen-tab')
+        Controller.home.classList.remove('chosen-tab')
+        Array.from(Controller.content.children).forEach(child=>content.removeChild(child))
+        Controller.menu.classList.add('chosen-tab')
+        Controller.content.appendChild(menuTab())
+    })
+
+    container.appendChild(menuOption)
+    let locationContainer = document.createElement('div')
+    locationContainer.classList.add('locationContainer')
+    locationContainer.textContent = 'Our location is 1234 Broad way , Europe'
+    container.appendChild(locationContainer)
     return container
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { menuTab } from './menu';
 import { contactTab } from './contact';
 
 //initial page 
-const content = document.querySelector('#content');
-const menu = document.querySelector('[class$=menu]');
-const home = document.querySelector('[class$=home]');
-const contact = document.querySelector('[class$=contact]');
-[menu,contact,home].forEach(tab=>{
+export const content = document.querySelector('#content');
+export const menu = document.querySelector('[class$=menu]');
+export const home = document.querySelector('[class$=home]');
+export const contact = document.querySelector('[class$=contact]');
+
+content.appendChild(homeTab())
+let arr =  [menu,contact,home]
+arr.forEach(tab=>{
     tab.addEventListener('click',()=>{
         menu.classList.remove('chosen-tab')
         contact.classList.remove('chosen-tab')
